d7992ed9766acacdd7ca8dc93c965f611bbd7724
Haik Voskanyan <voskanyanhaik@gmail.com>
2017-11-19 22:06:42 +0400

    Tabulation corrected

--- Script.js
+++ Script.js
@@ -1,21 +1,23 @@
 var cmd = 'gst-launch-1.0';
 
 var args = ['v4l2src', 'device=/dev/video0',
-    '!', 'video/x-raw,width=640,height=480,framerate=30/1',
-    '!', 'videoconvert',
-    '!', 'vp8enc', 'cpu-used=5', 'deadline=1', 'keyframe-max-dist=10',
-    '!', 'queue', 'leaky=1',
-    '!', 'tcpserversink', 'host=127.0.0.1', 'port=9001', 'sync-method=2'];
-
+			'!', 'video/x-raw, width=960,height=720, framerate=10/1',
+			'!', 'videoconvert',
+			'!', 'vp8enc', 'cpu-used=16', 'deadline=1', 'keyframe-max-dist=1',
+			'!', 'queue', 'leaky=1',
+			'!', 'm.', 'webmmux', 'name=m', 'streamable=true',
+			'!', 'tcpserversink', 'host=127.0.0.1', 'port=628', 'sync-method=2'];
 
 var child = require('child_process');
 var gstreamer = child.spawn(cmd, args, {stdio: 'inherit'});
 
 gstreamer.on('exit', function (code) {
-    if (code != null) {
+    if (code != null)
+	{
         console.log('GStreamer error, exit code ' + code);
-    }
-    process.exit();
+	}
+	
+	process.exit();
 });
 
 var express = require('express')
@@ -23,29 +25,30 @@ var app = express();
 var http = require('http')
 var httpServer = http.createServer(app);
 
-app.get('/', function (req, res) {
-    var date = new Date();
-
-    res.writeHead(200, {
-        'Date': date.toUTCString(),
-        'Connection': 'close',
-        'Cache-Control': 'private',
-        'Content-Type': 'video/webm',
-        'Server': 'CustomStreamer/0.0.1',
-    });
-
-    var net = require('net');
-    var socket = net.connect(628, function () {
-        socket.on('close', function (had_error) {
-            res.end();
-        });
-        socket.on('data', function (data) {
-            res.write(data);
-        });
-    });
-    socket.on('error', function (error) {
-        console.log(error);
-    });
-});
+app.get('/', function (req, res)
+{
+	var date = new Date();
+	
+	res.writeHead(200, {
+		'Date': date.toUTCString(),
+		'Connection': 'close',
+		'Cache-Control': 'private',
+		'Content-Type': 'video/webm',
+		'Server': 'CustomStreamer/0.0.1',
+	});
+	
+	var net = require('net');
+	var socket = net.connect(628, function () {
+		socket.on('close', function (had_error) {
+			res.end();
+		});
+		socket.on('data', function (data) {
+			res.write(data);
+		});
+	});
+	socket.on('error', function (error) {
+		console.log(error);
+	})
+;});
 
 httpServer.listen(314);
